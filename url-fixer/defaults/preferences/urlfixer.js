0def8c8c297c2c9e99194d53f5b18133f8e0f524
cfinke <cfinke@gmail.com>
2009-12-21 01:51:14 +0000

    Localization hook for the description

--- url-fixer/defaults/preferences/urlfixer.js
+++ url-fixer/defaults/preferences/urlfixer.js
@@ -1,1 +1,4 @@
+pref("extensions.{0fa2149e-bb2c-4ac2-a8d3-479599819475}.description", "chrome://url-fixer/locale/locale.properties");
+
 pref("extensions.url-fixer.askFirst", true);
+pref("extensions.url-fixer.version", "");
