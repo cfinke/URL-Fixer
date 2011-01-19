1bb7b6cd93eb940fb1ed416c30453ede02778261
Christopher Finke <cfinke@gmail.com>
2011-01-19 03:27:15 +0000

    Updated build script

--- build-xpi.sh
+++ build-xpi.sh
@@ -1,16 +1,11 @@
-rm url-fixer.xpi
+rm ~/Desktop/url-fixer.xpi
 rm -rf .tmp_xpi_dir/
 mkdir .tmp_xpi_dir/
 cp -r url-fixer/* .tmp_xpi_dir/
-
 rm -rf `find ./.tmp_xpi_dir/ -name ".DS_Store"`
 rm -rf `find ./.tmp_xpi_dir/ -name "Thumbs.db"`
 rm -rf `find ./.tmp_xpi_dir/ -name ".git"`
-
 cd .tmp_xpi_dir/
-zip -rq ../url-fixer.xpi *
+zip -rq ~/Desktop/url-fixer.xpi *
 cd ../
-
 rm -rf .tmp_xpi_dir/
-cp url-fixer.xpi ~/Desktop/
-
